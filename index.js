0633c58985180cd7c2acf8fb1bc289d0a6448729
Peppelina <proshekalnikovaa@gmail.com>
2022-03-31 19:58:59 +0300

    connect mongoose

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import express from 'express'
+import mongoose from "mongoose";
 
 const PORT = 5000
+const DB_URL = `mongodb+srv://root:root@cluster0.iomjm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json()) /*в ти json*/
 
-app.post('/', (req, res) => {
-    console.log(req.body)
+app.get('/', (req, res) => {
     res.status(200).json('Сервер работает')
 })
 
-app.listen(PORT, () => console.log('сервер работает ' + PORT))
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log('сервер работает ' + PORT))
+    }
+    catch(e) {
+        console.log(e)
+    }
+}
+
+startApp()
+
