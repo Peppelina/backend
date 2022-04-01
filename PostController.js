0e98ba8275ed602a37ddc521dc04f9263c8f3828
Peppelina <proshekalnikovaa@gmail.com>
2022-04-01 17:16:01 +0300

    add CRUD endpoints

---
+++ PostController.js
@@ -0,0 +1,68 @@
+import Post from "./Post.js";
+
+class PostController{
+    static async create(req, res) {
+        try {
+            const {author, title, content, picture} = req.body
+            const post = await Post.create({author, title, content, picture})
+            res.json(post)
+        }
+        catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    static async getAll(req, res) {
+        try {
+            const posts = await Post.find();
+            return res.json(posts)
+        }
+        catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    static async getOne(req, res) {
+        try {
+            const {id} = req.params
+            if (!id) {
+                res.status(400).json({message: "ID не указан"})
+            }
+            const post = await Post.findById(id)
+            return res.json(post)
+        }
+        catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    static async update(req, res) {
+        try {
+            const post = req.body
+            if (!post._id) {
+                res.status(400).json({message: "ID не указан"})
+            }
+            const updatePost = await Post.findByIdAndUpdate(post._id, post, {new:true})
+            return res.json(updatePost)
+        }
+        catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    static async delete(req, res) {
+        try {
+            const {id} = req.params
+            if (!id) {
+                res.status(400).json({message: "ID не указан"})
+            }
+            const post = await Post.findByIdAndDelete(id)
+            return res.json(post)
+        }
+        catch (e) {
+            res.status(500).json(e)
+        }
+    }
+}
+
+export default PostController
